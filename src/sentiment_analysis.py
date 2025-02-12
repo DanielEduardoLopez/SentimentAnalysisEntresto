b6dfee5fd543ddacfc5af59d2fc6af72fbe4ae1f
Daniel Eduardo López <delopmar@gmail.com>
2025-02-12 16:52:14 -0600

    Added function to get subjectivity

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -2,23 +2,45 @@
 Collection of functions for sentiment analysis using TextBlob.
 """
 
+# Libraries importation
 from textblob import TextBlob
 
+# Functions
 def get_polarity(text: str) -> float:
     """
-    Returns the polarity of a text within a range [-1.0, 1.0] using TextBlob.
+    Returns the polarity of a text within a range [-1.0, 1.0] using TextBlob, where:    
+       -1.0: Negative
+        0.0: Neutral
+        1.0: Positive
 
     Parameters:
     text (str): Input text.
 
     Returns:
-    polarity (float): Polarity of the text within the range [-1.0, 1.0], where:
-        -1.0: Negative
-        0.0: Neutral
-        1.0: Positive
+    polarity (float): Polarity of the text within the range [-1.0, 1.0].
 
     """
 
     polarity = TextBlob(text).sentiment.polarity
 
     return polarity
+
+
+def get_subjectivity(text: str) -> float:
+    """
+    Returns the subjectivity of a text within a range [0.0, 1.0] using TextBlob, where:       
+        0.0: Objective
+        1.0: Subjective
+
+    Parameters:
+    text (str): Input text.
+
+    Returns:
+    subjectivity (float): Polarity of the text within the range [-1.0, 1.0], where:
+
+
+    """
+
+    subjectivity = TextBlob(text).sentiment.subjectivity
+
+    return subjectivity
